a0641bcf5160122130fd910548663c92e9b54519
Suzanna Filipowska <suzanna.filipowska@gmail.com>
2020-08-25 14:38:10 +0200

    added img to dish

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -1,5 +1,5 @@
 class Dish < ApplicationRecord
   belongs_to :menu
   has_many :events, through: :menus
-  validates :name, :description, presence: true
+  validates :name, :description, :image, presence: true
 end
