4f6a1626729864c28299d83f207fb7f2722736f7
Sandra Porqueres <sandraporqueres@gmail.com>
2020-08-31 12:19:55 +0200

    private

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,17 @@
 class ReviewsController < ApplicationController
+  def create
+    @booking = Booking.find(params[:booking_id])
+    @review = Review.new(review_params)
+    @review.booking = @booking
+
+    if @review.save
+      redirect_to_booking_path(@booking)
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:number_people)
+  end
 end
