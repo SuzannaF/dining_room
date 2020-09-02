0198cea21c2836075d2bfd43a2f8691083f2383c
Sandra Porqueres <sandraporqueres@gmail.com>
2020-09-02 09:22:51 +0200

    key

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,6 +3,6 @@ class Review < ApplicationRecord
   validates :rating, :comment, :rating_host, presence: true
   # removed comment validation 20 caracters are too much.
   # validates :comment, length: { minimum: 20 }
-  validates :rating, inclusion: { in: (1..5), message: "%{rating} should be a number between 1 and 5" }
-  validates :rating_host, inclusion: { in: (1..5), message: "%{rating} should be a number between 1 and 5" }
+  validates :rating, inclusion: { in: (1..5), message: "Rating should be a number between 1 and 5" }
+  validates :rating_host, inclusion: { in: (1..5), message: "Rating a host should be a number between 1 and 5" }
 end
